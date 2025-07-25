25b4835f0b6f1b7691ae681f4d6ff45900531fbd
GotLib567 <gotlibweb@mail.ru>
2025-07-25 17:25:59 +0500

    added upload form and file upload component

--- app/routes/upload.tsx
+++ app/routes/upload.tsx
@@ -1,12 +1,27 @@
 import {type FormEvent, useState} from 'react';
 import Navbar from "../../components/Navbar";
+import FileUploader from "../../components/FileUploader";
 
 const Upload = () => {
     const [isProcessing, setIsProcessing] = useState(false);
     const [statusText, setStatusText] = useState('');
+    const [file, setFile] = useState<File | null>(null);
+
+    const handleFileSelect = (file: File | null) => {
+        setFile(file);
+    }
 
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const form = e.currentTarget.closest('form');
+        if (!form) return;
+        const formData = new FormData(form);
 
+        const companyName = formData.get('company-name');
+        const jobTitle = formData.get('job-title');
+        const jobDescription = formData.get('job-description');
+
+        console.log({companyName, jobTitle, jobDescription, file});
     }
 
     return (
@@ -40,7 +55,7 @@ const Upload = () => {
                             </div>
                             <div className="form-div">
                                 <label htmlFor="uploader">Upload Resume</label>
-                                <div>Uploader</div>
+                                <FileUploader onFileSelect={handleFileSelect} />
                             </div>
 
                             <button className="primary-button" type="submit">
